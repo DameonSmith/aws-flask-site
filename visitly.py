751e5686129a0fa1bdd04b1a24dad837f8f02874
Dameon Smith <dameonsmith76@gmail.com>
2018-06-26 12:30:50 -0600

    Added health check route

--- visitly.py
+++ visitly.py
@@ -5,5 +5,9 @@ application = Flask(__name__)
 def hello():
     return "<h1 style='color:blue'>Hello There!</h1>"
 
+@application.route("/health")
+def hello():
+    return "<h1 style='color:blue'>Application is Healthy!</h1>"
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
